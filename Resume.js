7bc157f64f4679710a33e0d0cb401f1d7c1f1a68
alexsuh22 <alexsuh22@yahoo.com>
2016-10-28 14:27:31 -0700

    Update Resume.js

--- Resume.js
+++ Resume.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     $('#img').click(function() {
-		$(this).effect('explode');
+		$(this).toggle('explode');
 	    
 	    
     });
 	
 	
 	$('#addy').click(function() {
-		$(this).effect('bounce', {times:5}, 500);
+		$(this).toggle('bounce', {times:5}, 500);
 		
 		
 		
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	
 	
 	$('h4').click(function() {
-		$(this).effect('slide', {times:3}, 500);
+		$(this).toggle('slide', {times:3}, 500);
     });
 	
 });
